a2e38e7789c9e7ebb9e0eb959e15b0439bdb6461
Siddhartha kanakagiri <ksiddus@users.noreply.github.com>
2024-10-17 16:02:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-# Import python packages
+# Import Python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
 import requests
 from snowflake.snowpark.functions import col
 
